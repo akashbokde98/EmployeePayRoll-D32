d2e0d84377f696113073d855ea03bd6c8c9cb0b6
akashbokde98 <akashbokde98@gmail.com>
2023-03-06 00:45:09 +0530

    Added:Initial Push

---
+++ EmpPayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpPayRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Pay Roll Problem!");
+        }
+    }
+}
